dc4ab30a34b4e9806c88776f5f2cc098d40c1e5b
José Muñoz <jose.munoz.dev@protonmail.com>
2022-03-19 12:01:37 -0700

    `onHWKeyPressed` adjustment of event handler type
    currently `onHWKeyPressed` receives an object instead of an event handler, I've updated the types based on the automated ts errors presented by the current type declaration

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,6 @@
 declare module "react-native-hw-keyboard-event";
 
-export function onHWKeyPressed(hwKeyEvent: { pressedKey: string }): void;
+type keyboardEventHandler = (evt: { pressedKey: string }) => void
+
+export function onHWKeyPressed(hwKeyEvent: keyboardEventHandler): void;
 export function removeOnHWKeyPressed(): void;
